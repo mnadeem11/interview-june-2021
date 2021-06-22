9718ca8d714ff1396fc5e75cceadd15a5cfd2a74
mnadeem11 <86279791+mnadeem11@users.noreply.github.com>
2021-06-22 13:17:54 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,10 +1,14 @@
-package exam;
-
-public class ShiftElementsInArray {
-
-  public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-
-  }
-
-}
+class Arr Replce{
+  public static void main(string[] args){
+    int[] arr=new inr[5];
+    arr[0]=1;
+    arr[1]=2;
+    arr[2]=3;
+    arr[3]=4;
+    arr[4]=5;
+    int first=arr[0];
+    int last=arr[5-1];  
+    for(int i=0;i<=4;i++){
+      system.out.println(arr[i]);
+    }
+  } 
